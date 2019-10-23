0d64a250e45e69cc482b0287890f5cd50f88eca7
César García Acebal <cesarga@d3riva.com>
2019-10-23 16:15:20 +0200

    WIP

--- 00-install/02-python/run_once_00-install.sh
+++ 00-install/02-python/run_once_00-install.sh
@@ -10,10 +10,5 @@ python3-pip
 EOF
 )
 
-sudo update-alternatives --install \
-    "$(command -v python3)" python3 \
-    "$(command -v "python$VERSION")" 1
-
-sudo update-alternatives --install \
-    "$(command -v python)" python \
-    "$(command -v "python$VERSION")" 1
+sudo update-alternatives --install "$(command -v python3)" python3 "$(command -v "python$VERSION")" 1
+sudo update-alternatives --install "$(command -v python)" python "$(command -v "python$VERSION")" 1
