f6c50403e5e278f2dbf9d1f9c2b6ae27b77748b2
César García Acebal <cesarga@d3riva.com>
2019-10-23 23:40:12 +0200

    WIP

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,10 +11,12 @@ fi
 chezmoi init https://github.com/cesarga/dotfiles.git
 #chezmoi source -- submodule update --init --recursive
 
-curl -s -L -o /tmp/zplugin-v2.3.tar.gz https://github.com/zdharma/zplugin/archive/v2.3.tar.gz
-chezmoi import --strip-components 1 --destination "$HOME/libs/zplugin/bin" /tmp/zplugin-v2.3.tar.gz
-
+mkdir -p "$HOME/libs"
 curl -s -L -o /tmp/tpm-master.tar.gz https://github.com/tmux-plugins/tpm/archive/master.tar.gz
 chezmoi import --strip-components 1 --destination "$HOME/libs/tpm" /tmp/tpm-master.tar.gz
+
+mkdir -p "$HOME/libs/zplugin"
+curl -s -L -o /tmp/zplugin-v2.3.tar.gz https://github.com/zdharma/zplugin/archive/v2.3.tar.gz
+chezmoi import --strip-components 1 --destination "$HOME/libs/zplugin/bin" /tmp/zplugin-v2.3.tar.gz
 
 chezmoi apply
