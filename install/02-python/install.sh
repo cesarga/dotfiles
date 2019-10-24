127e61a5521b8b72d29145e1587b3a439262ee6a
César García Acebal <cesarga@d3riva.com>
2019-10-24 02:27:58 +0200

    WIP

--- install/02-python/install.sh
+++ install/02-python/install.sh
@@ -1,26 +1,31 @@
 #!/usr/bin/env bash
 
-VERSION='3.7'
+#VERSION='3.6'
 
-if [[ ! "$(command -v python3.7)" ]]; then
-
-  # Install python3 and pip using apt
+if [[ ! "$(command -v "python$VERSION")" ]]; then
   sudo DEBIAN_FRONTEND=noninteractive apt-get install -y $(cat <<-EOF
-		python${VERSION}
+		python3
 		python3-pip
 	EOF
-)
+	)
 
-  # Update alternatives for python3 and `python` binaries
-  if [[ "$(command -v python3)" ]]; then
-      sudo update-alternatives --install "$(command -v python3)" python3 "$(command -v "python$VERSION")" 1
-  fi
-
-  if [[ "$(command -v python)" ]]; then
-      sudo update-alternatives --install "$(command -v python)" python "$(command -v "python$VERSION")" 1
-  fi
-
-  # Fix missing `apt_pkg` error when running `apt-get update`
-  sudo apt-get remove -y python3-apt
-  sudo apt-get install -y python3-apt
+#  # Install python3 and pip using apt
+#  sudo DEBIAN_FRONTEND=noninteractive apt-get install -y $(cat <<-EOF
+#		python${VERSION}
+#		python3-pip
+#	EOF
+#)
+#
+#  # Update alternatives for python3 and `python` binaries
+#  if [[ "$(command -v python3)" ]]; then
+#      sudo update-alternatives --install "$(command -v python3)" python3 "$(command -v "python$VERSION")" 1
+#  fi
+#
+#  if [[ "$(command -v python)" ]]; then
+#      sudo update-alternatives --install "$(command -v python)" python "$(command -v "python$VERSION")" 1
+#  fi
+#
+#  # Fix missing `apt_pkg` error when running `apt-get update`
+#  sudo apt-get remove -y python3-apt
+#  sudo apt-get install -y python3-apt
 fi
