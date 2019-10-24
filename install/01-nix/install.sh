59f7e72b826eba816494fc97df4e4ce1d9927000
César García Acebal <cesarga@d3riva.com>
2019-10-24 02:04:06 +0200

    WIP

--- install/01-nix/install.sh
+++ install/01-nix/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ noexists-exec 'nix-env' ]]; then
+if noexists-exec 'nix-env'; then
   sudo install -d -m755 -o $(id -u) -g $(id -g) /nix
   curl https://nixos.org/nix/install | sh
   source "$HOME/.nix-profile/etc/profile.d/nix.sh"
