d05c2df56e716ac5a869f11b253bdc4e100d7739
César García Acebal <cesarga@d3riva.com>
2019-10-25 04:35:22 +0200

    WIP

--- install/08-ssh/install.sh
+++ install/08-ssh/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if noexists-exec '/c/weasel-pageant-helper.exe'; then
-  cp "$HOME/libs/weasel-pageant/helper.exe" '/c/weasel-pageant-helper.exe'
+  sudo cp "$HOME/libs/weasel-pageant/helper.exe" '/c/weasel-pageant-helper.exe'
 fi
