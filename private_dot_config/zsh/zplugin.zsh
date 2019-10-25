35dd97a3ac3f93913840eae5ea588f9e7e1da060
César García Acebal <cesarga@d3riva.com>
2019-10-25 14:19:33 +0200

    WIP

--- private_dot_config/zsh/zplugin.zsh
+++ private_dot_config/zsh/zplugin.zsh
@@ -42,7 +42,7 @@ zplugin snippet OMZ::plugins/encode64/encode64.plugin.zsh
 zplugin snippet OMZ::lib/history.zsh
 zplugin snippet OMZ::plugins/history-substring-search/history-substring-search.plugin.zsh
 
-zplugin snippet OMZ::lib/key-bindings.zsh
+#zplugin snippet OMZ::lib/key-bindings.zsh
 
 zplugin snippet https://github.com/changyuheng/zsh-interactive-cd/blob/master/zsh-interactive-cd.plugin.zsh
 
