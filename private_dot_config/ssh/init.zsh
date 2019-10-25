308bd9544204ce71777e9a73ded37f996966b5e5
César García Acebal <cesarga@d3riva.com>
2019-10-25 05:15:46 +0200

    WIP

--- private_dot_config/ssh/init.zsh
+++ private_dot_config/ssh/init.zsh
@@ -1,3 +1,3 @@
-if [[ $(command -v '/c/weasel-pageant-helper.exe' ) ]]; then
+if [[ $(command -v '/z/.wsl/weasel-pageant-1.4/helper.exe' ) ]]; then
   eval $("$HOME/libs/weasel-pageant/weasel-pageant" -b -r -a '/tmp/S.weasel-pageant' -H '/z/.wsl/weasel-pageant-1.4/helper.exe') 2>&1 >/dev/null
 fi
