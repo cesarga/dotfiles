49082be63f8e94c702782f1d219272748e3a1b4c
César García Acebal <cesarga@d3riva.com>
2019-10-25 12:39:57 +0200

    WIP

--- private_dot_config/private_gpg/init.zsh
+++ private_dot_config/private_gpg/init.zsh
@@ -1,3 +1,3 @@
- if [[ ! -S "$GNUPGHOME/S.gpg-agent" ]]; then
-  (exec socat UNIX-LISTEN:"$GNUPGHOME/S.gpg-agent,fork" EXEC:"npiperelay.exe -ep -ei -s 'C:/Users/Cesar/AppData/Roaming/gnupg/S.gpg-agent'",nofork &)
- fi
+# if [[ ! -S "$GNUPGHOME/S.gpg-agent" ]]; then
+#  (exec socat UNIX-LISTEN:"$GNUPGHOME/S.gpg-agent,fork" EXEC:"npiperelay.exe -ep -ei -s 'C:/Users/Cesar/AppData/Roaming/gnupg/S.gpg-agent'",nofork &)
+# fi
