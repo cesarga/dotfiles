a465e72d093ec0eb4eba825c41fc4ed3ca498084
César García Acebal <cesarga@d3riva.com>
2019-10-28 17:33:57 +0100

    WIP

--- private_dot_config/ssh/init.zsh
+++ private_dot_config/ssh/init.zsh
@@ -1,5 +1,1 @@
-if [[ ! -S "$GNUPGHOME/S.gpg-agent.ssh" ]]; then
-  (exec socat UNIX-LISTEN:"$GNUPGHOME/S.gpg-agent.ssh,fork" EXEC:"npiperelay.exe -ep -ei -s 'C:/Users/Cesar/AppData/Roaming/gnupg/S.gpg-agent.ssh'",nofork &)
-fi
-
 eval $("$HOME/libs/weasel-pageant/weasel-pageant" -rb -a '/tmp/S.weasel-pageant' -H '/z/.wsl/weasel-pageant-1.4/helper.exe') 2>&1 >/dev/null
