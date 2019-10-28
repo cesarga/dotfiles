36a38b393d397c301ed15f992f085db31df0a961
César García Acebal <cesarga@d3riva.com>
2019-10-28 17:27:35 +0100

    WIP

--- private_dot_config/private_gpg/executable_gpg-agent-relay.sh
+++ private_dot_config/private_gpg/executable_gpg-agent-relay.sh
@@ -2,13 +2,12 @@
 # Launches socat+npiperelay to relay the gpg-agent socket file for use in WSL
 # See https://justyn.io/blog/using-a-yubikey-for-gpg-in-windows-10-wsl-windows-subsystem-for-linux/ for details
 
-GPGDIR="${GNUPGHOME}"
 USERNAME=Cesar
 # I use the same username for wsl and windows, but feel free to modify the paths below if that isn't the case
-WIN_GPGDIR="C:/Users/${USERNAME}/AppData/Roaming/gnupg"
+WIN_GNUPGHOME="C:/Users/${USERNAME}/AppData/Roaming/gnupg"
 #NPIPERELAY="/c/Users/${USERNAME}/bin/npiperelay.exe"
 NPIPERELAY="npiperelay.exe"
-PIDFILE="${GPGDIR}/.gpg-agent-relay.pid"
+PIDFILE="${GNUPGHOME}/.gpg-agent-relay.pid"
 OLDPID=$(cat "${PIDFILE}")
 
 # Launch socat+npiperelay for the regular gpg-agent
@@ -18,14 +17,14 @@ if [ ! -z "${OLDPID}" ]; then
     exit 0
 fi
 
-rm -f "${GPGDIR}/S.gpg-agent*"
+rm -f "${GNUPGHOME}/S.gpg-agent*"
 echo $$ > ${PIDFILE}
 
 # Relay the regular gpg-agent socket for gpg operations
 (exec socat UNIX-LISTEN:"$GNUPHOME/S.gpg-agent,fork" EXEC:"npiperelay.exe -ep -ei -s 'C:/Users/Cesar/AppData/Roaming/gnupg/S.gpg-agent'",nofork &)
 
 # Relay the gpg ssh-agent
-#socat UNIX-LISTEN:"${GPGDIR}/S.gpg-agent.ssh,fork" EXEC:"${NPIPERELAY} -ep -ei -s -a '${WIN_GPGDIR}/S.gpg-agent.ssh'",nofork &
+#socat UNIX-LISTEN:"${GNUPGHOME}/S.gpg-agent.ssh,fork" EXEC:"${NPIPERELAY} -ep -ei -s -a '${WIN_GNUPGHOME}/S.gpg-agent.ssh'",nofork &
 SSHPID=$!
 
 wait ${AGENTPID}
