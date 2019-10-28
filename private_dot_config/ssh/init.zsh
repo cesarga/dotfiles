5442f16500ac963bb1c81cb873c4518c9a9df09f
César García Acebal <cesarga@d3riva.com>
2019-10-28 18:08:43 +0100

    WIP

--- private_dot_config/ssh/init.zsh
+++ private_dot_config/ssh/init.zsh
@@ -1,1 +1,1 @@
-eval $("$HOME/libs/weasel-pageant/weasel-pageant" -rb -a '/tmp/S.weasel-pageant' -H '/z/.wsl/weasel-pageant-1.4/helper.exe') 2>&1 >/dev/null
+eval $('/z/.wsl/weasel-pageant-1.4/weasel-pageant' -b -r -a '/tmp/S.weasel-pageant') 2>&1 >/dev/null
