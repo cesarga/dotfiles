923a9277b27399919e30399e77bac7c02aee9b10
César García Acebal <cesarga@d3riva.com>
2019-10-28 19:10:19 +0100

    WIP

--- private_dot_config/ssh/init.zsh
+++ private_dot_config/ssh/init.zsh
@@ -1,1 +1,2 @@
-eval $('/z/.wsl/weasel-pageant-1.4/weasel-pageant' -b -r -a '/tmp/S.weasel-pageant') 2>&1 >/dev/null
+#eval $('/z/.wsl/weasel-pageant-1.4/weasel-pageant' -b -r -a '/tmp/S.weasel-pageant') 2>&1 >/dev/null
+eval $(/z/.wsl/ssh-agent-wsl/ssh-agent-wsl -r -b)
