dd4d1c28c1e353b7a88452f49f33394f0f2582dd
César García Acebal <cesarga@d3riva.com>
2019-10-29 05:16:52 +0100

    WIP

--- private_dot_config/ssh/init.zsh
+++ private_dot_config/ssh/init.zsh
@@ -1,9 +1,9 @@
 eval $(/z/.wsl/weasel-pageant-1.4/weasel-pageant -b -r -a '/tmp/S.weasel-pageant') 2>&1 >/dev/null
 
-if [[ ! -f "$GNUPGHOME/S.gpg-agent" ]]; then
-  (exec socat UNIX-LISTEN:"$GNUPGHOME/S.gpg-agent,fork" EXEC:"C:/Users/cesar/go/bin/npiperelay.exe -ep -ei -s 'C:/Users/cesar/AppData/Roaming/gnupg/S.gpg-agent'",nofork &)
-fi
+#if [[ ! -f "$GNUPGHOME/S.gpg-agent" ]]; then
+#  (exec socat UNIX-LISTEN:"$GNUPGHOME/S.gpg-agent,fork" EXEC:"C:/Users/cesar/go/bin/npiperelay.exe -ep -ei -s 'C:/Users/cesar/AppData/Roaming/gnupg/S.gpg-agent'",nofork &)
+#fi
 
-if [[ ! -f "$GNUPGHOME/S.gpg-agent.ssh" ]]; then
-  (exec socat UNIX-LISTEN:"$GNUPGHOME/S.gpg-agent.ssh,fork" EXEC:"C:/Users/cesar/go/bin/npiperelay.exe -ep -ei -s 'C:/Users/cesar/AppData/Roaming/gnupg/S.gpg-agent.ssh'",nofork &)
-fi
+#if [[ ! -f "$GNUPGHOME/S.gpg-agent.ssh" ]]; then
+#  (exec socat UNIX-LISTEN:"$GNUPGHOME/S.gpg-agent.ssh,fork" EXEC:"C:/Users/cesar/go/bin/npiperelay.exe -ep -ei -s 'C:/Users/cesar/AppData/Roaming/gnupg/S.gpg-agent.ssh'",nofork &)
+#fi
