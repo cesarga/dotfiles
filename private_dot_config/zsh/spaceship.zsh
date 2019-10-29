6f7d283ffd804a3852c7af7486cb91beb593a965
César García Acebal <cesarga@d3riva.com>
2019-10-29 02:56:05 +0100

    WIP

--- private_dot_config/zsh/spaceship.zsh
+++ private_dot_config/zsh/spaceship.zsh
@@ -5,34 +5,34 @@
 
 # Order (left)
 export SPACESHIP_PROMPT_ORDER=(
-#  time          # Time stamps section
+  time          # Time stamps section
 #  user          # Username section
   dir           # Current directory section
 #  host          # Hostname section
   git           # Git section (git_branch + git_status)
 #  hg            # Mercurial section (hg_branch  + hg_status)
-  package       # Package version
-  node          # Node.js section
+#  package       # Package version
+#  node          # Node.js section
 #  ruby          # Ruby section
 #  elixir        # Elixir section
 #  xcode         # Xcode section
 #  swift         # Swift section
 #  golang        # Go section
-  php           # PHP section
+#  php           # PHP section
 #  rust          # Rust section
 #  haskell       # Haskell Stack section
 #  julia         # Julia section
-  docker        # Docker section
-  aws           # Amazon Web Services section
-  venv          # virtualenv section
+#  docker        # Docker section
+#  aws           # Amazon Web Services section
+#  venv          # virtualenv section
 #  conda         # conda virtualenv section
 #  pyenv         # Pyenv section
 #  dotnet        # .NET section
 #  ember         # Ember.js section
-  kubecontext   # Kubectl context section
-  terraform     # Terraform workspace section
+#  kubecontext   # Kubectl context section
+#  terraform     # Terraform workspace section
   exec_time     # Execution time
-  line_sep      # Line break
+#  line_sep      # Line break
 #  battery       # Battery level and status
 #  vi_mode       # Vi-mode indicator
   jobs          # Background jobs indicator
@@ -42,7 +42,7 @@ export SPACESHIP_PROMPT_ORDER=(
 
 # Order (right)
 export SPACESHIP_RPROMPT_ORDER=(
-  time          # Time stamps section
+#  time          # Time stamps section
 #  user          # Username section
 #  dir           # Current directory section
 #  host          # Hostname section
@@ -115,44 +115,3 @@ export SPACESHIP_KUBECONTEXT_COLOR=057
 
 # Execution time
 export SPACESHIP_EXEC_TIME_COLOR=227
-
-#
-# Disabled modules
-#
-export SPACESHIP_HOST_SHOW=false
-
-export SPACESHIP_HG_SHOW=false
-export SPACESHIP_HG_BRANCH_SHOW=false
-export SPACESHIP_HG_STATUS_SHOW=false
-
-export SPACESHIP_RUBY_SHOW=false
-
-export SPACESHIP_ELM_SHOW=false
-
-export SPACESHIP_ELIXIR_SHOW=false
-
-export SPACESHIP_XCODE_SHOW_LOCAL=false
-export SPACESHIP_XCODE_SHOW_GLOBAL=false
-
-export SPACESHIP_SWIFT_SHOW_LOCAL=false
-export SPACESHIP_SWIFT_SHOW_GLOBAL=false
-
-export SPACESHIP_GOLANG_SHOW=false
-
-export SPACESHIP_RUST_SHOW=false
-
-export SPACESHIP_HASKELL_SHOW=false
-
-export SPACESHIP_JULIA_SHOW=false
-
-export SPACESHIP_CONDA_SHOW=false
-
-export SPACESHIP_PYENV_SHOW=false
-
-export SPACESHIP_DOTNET_SHOW=false
-
-export SPACESHIP_EMBER_SHOW=false
-
-export SPACESHIP_BATTERY_SHOW=false
-
-export SPACESHIP_VI_MODE_SHOW=false
