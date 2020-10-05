3e6e7cbe74b41c93aba6fba567a117a08428c06f
César García Acebal <cesarga@d3riva.com>
2020-10-05 14:44:49 +0200

    WIP

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,9 +8,9 @@ if [[ ! "$(command -v chezmoi)" ]]; then
   curl -sfL https://git.io/chezmoi | sudo BINDIR=/usr/local/bin sh
 fi
 
-#chezmoi init https://github.com/cesarga/dotfiles.git
-mkdir -p "$HOME/.local/share/chezmoi"
-sudo mount -o bind /z/dev/projects/dotfiles/ubuntu-20.04/ "$HOME/.local/share/chezmoi"
+chezmoi init https://github.com/cesarga/dotfiles.git
+#mkdir -p "$HOME/.local/share/chezmoi"
+#sudo mount -o bind /z/dev/projects/dotfiles/ubuntu-20.04/ "$HOME/.local/share/chezmoi"
 
 # weasel-pageant
 #curl -s -L -o /tmp/weasel-pageant-1.4.tar.gz https://raw.githubusercontent.com/cesarga/dotfiles/master/exports/weasel-pageant-1.4.tar.gz
@@ -18,6 +18,6 @@ sudo mount -o bind /z/dev/projects/dotfiles/ubuntu-20.04/ "$HOME/.local/share/ch
 
 # tpm
 curl -s -L -o /tmp/tpm-master.tar.gz https://github.com/tmux-plugins/tpm/archive/master.tar.gz
-chezmoi import --strip-components 1 --destination "$HOME/libs/tpm"  /tmp/tpm-master.tar.gz
+/usr/local/bin/chezmoi import --strip-components 1 --destination "$HOME/libs/tpm"  /tmp/tpm-master.tar.gz
 
-chezmoi apply
+/usr/local/bin/chezmoi apply
