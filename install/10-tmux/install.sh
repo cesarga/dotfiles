43f83a585c4f1895a47563067eb5f9ad8e2f37e7
César García Acebal <cesarga@d3riva.com>
2020-10-06 01:37:32 +0200

    WIP

--- install/10-tmux/install.sh
+++ install/10-tmux/install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-wget https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.zsh -O /usr/local/share/zsh/site-functions/_tmuxinator
+sudo wget https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.zsh -O /usr/local/share/zsh/site-functions/_tmuxinator
