a6e7821b9bb18c9b76f71281cf86917f7d9c6bdb
César García Acebal <cesarga@d3riva.com>
2020-10-06 08:18:07 +0200

    WIP

--- private_dot_config/zsh/options.zsh
+++ private_dot_config/zsh/options.zsh
@@ -3,7 +3,7 @@ setopt AUTOPUSHD PUSHDMINUS PUSHDSILENT PUSHDTOHOME
 setopt AUTOCD	# cd by typing dirname
 setopt CDABLEVARS	# follow variables which are dirnames
 setopt MULTIOS # allow multiple redirection echo 'a'>b>c
-setopt CORRECT CORRECT_ALL # try to correct command line spelling
+#setopt CORRECT CORRECT_ALL # try to correct command line spelling
 setopt NOHUP # don't HUP running jobs on logout
 setopt EXTENDED_GLOB # enable extended globbing
 setopt INTERACTIVE_COMMENTS # allow comments on cmd line
@@ -16,7 +16,7 @@ setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
 setopt COMPLETE_ALIASES # don't expand aliases _before_ completion has finished (like: git comm-[tab])
 
-unsetopt NOMATCH # allow [ or ] whereever you want
+nsetopt NOMATCH # allow [ or ] whereever you want
 
 #
 # History
